81578f651bc8240ca30bf110f6b2d2f90c4e8fc0
shanto-swe029 <ariful.shanto786@gmail.com>
2023-07-02 12:46:44 +0600

    working with click listener

--- Client/script.js
+++ Client/script.js
@@ -50,11 +50,37 @@ function renderTodos() {
             <div class="todo" id= ${index}>
                 <img 
                     src="${todo.checked ? 'img/circle-marked.png' : 'img/circle-unmarked.png'}" 
-                    class="img-icon-small">
-                <p class="">${todo.value}</p>
-                <img src="img/edit.png" class="img-icon-small">
-                <img src="img/trash-red.png" class="img-icon-small">
+                    class="img-icon-small"
+                    data-action="check">
+                <p class="" data-action="check">${todo.value}</p>
+                <img src="img/edit.png" 
+                    class="img-icon-small"
+                    data-action="edit">
+                <img src="img/trash-red.png" 
+                    class="img-icon-small"
+                    data-action="delete">
             </div>
         `
     })
 }
+
+// CLICK EVENT LISTENER FOR ALL THE TODOS
+todosListEl.addEventListener('click', (event) => {
+    const target = event.target;
+    const parentElement = target.parentElement;
+
+    if( parentElement.className !== 'todo' ) return;
+
+    // TODO ID
+    const todo = parentElement;
+    const todoId = Number(todo.id);
+
+    // Target Action
+    const action = target.dataset.action;
+
+    action === "check" && checkTodo(todoId);
+    action === "edit" && editTodo(todoId);
+    action === "delete" && deleteTodo(todoId);
+});
+
+// CHECK A TODO
