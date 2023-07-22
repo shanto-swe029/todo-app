b3bf413f086a0e91edae27c2a903f98aae14659a
shanto-swe029 <ariful.shanto786@gmail.com>
2023-07-22 16:25:32 +0600

    Create Database Connection

---
+++ REST Api/connection.js
@@ -0,0 +1,13 @@
+// Create Database Connection
+
+const {Client} = require('pg')
+
+const client = new Client({
+    host: "localhost",
+    user: "postgres",
+    port: 5432,
+    password: "rootUser",
+    database: "postgres"
+})
+
+module.exports = client
