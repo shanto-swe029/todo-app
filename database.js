ac327b0612c5a5b3e2f3800728b91bbbc1e9079a
shanto-swe029 <ariful.shanto786@gmail.com>
2023-07-24 03:00:57 +0600

    updated database.js file

--- database.js
+++ database.js
@@ -1,7 +1,5 @@
 const mysql = require('mysql2');
-
 const dotenv = require('dotenv');
-
 dotenv.config();
 
 const pool = mysql.createPool({
@@ -11,37 +9,12 @@ const pool = mysql.createPool({
     password: '1234',
     waitForConnections: true,
     connectionLimit: 10,
-    maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
-    idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+    maxIdle: 10,
+    idleTimeout: 60000,
     queueLimit: 0,
     enableKeepAlive: true,
     keepAliveInitialDelay: 0
   }).promise();
-
-  // For pool initialization, see above
-// pool.getConnection(function(err, conn) {
-//     // Do something with the connection
-//     console.log("balsal",err);
-//     // Don't forget to release the connection when finished!
-//     pool.releaseConnection(conn);
-//   })
-
-
-// const createTaskTable = async () => {
-//     let sql = `
-//     CREATE TABLE tasks(
-//         taskId VARCHAR(200) PRIMARY KEY,
-//         taskName VARCHAR(200)
-//     );
-//     `
-
-//     return await pool.query(sql);
-// }
-
-
-// createTaskTable().then((val) => {
-//     console.log(val);
-// })
 
 const showTasks = async (email) => {
     let sql = `
@@ -51,11 +24,6 @@ const showTasks = async (email) => {
     `;
     return await pool.query(sql, [email]);
 }
-
-// showTasks().then((val) => {
-//     const [row] = val;
-//     console.log(row);
-// })
 
 const addTask = async (taskId, taskName, email) => {
     let sql = `
@@ -68,14 +36,6 @@ const addTask = async (taskId, taskName, email) => {
     return something;
 }
 
-// addTask('radme2430', 'This is my 2nd Task').then((val) => {
-//     console.log(val);
-// })
-
-// addTask('sadme2340', 'this is my 1st Task').then((val) => {
-//     console.log(val);
-// })
-
 const deleteTask = async (taskId) => {
     let sql = `
         DELETE FROM tasks
@@ -84,11 +44,6 @@ const deleteTask = async (taskId) => {
 
     return await pool.query(sql, taskId);
 }
-
-
-// deleteTask('radme2430').then((val) => {
-//     console.log(val);
-// })
 
 const editTask = async (taskId, taskName) => {
     console.log(taskId,taskName);
@@ -101,10 +56,6 @@ const editTask = async (taskId, taskName) => {
     return await pool.query(sql, [taskName, taskId]);
 }
 
-// editTask('radme2430', 'This is my edited 2nd task').then((val) => {
-//     console.log(val);
-// })
-
 const getTaskName = async (id) => {
     let sql = `
         SELECT taskName 
@@ -114,10 +65,6 @@ const getTaskName = async (id) => {
     return await pool.query(sql, id);
 }
 
-// getTaskName('radme2430').then((val) => {
-//     console.log(val[0][0]);
-// })
-
 const addUser = async (username, email, hashedPassword) => {
     let sql = `
         INSERT INTO users
@@ -126,7 +73,6 @@ const addUser = async (username, email, hashedPassword) => {
 
     return await pool.query(sql);
 }
-
 
 const getUser = async (email) => {
     console.log('check');
@@ -138,7 +84,5 @@ const getUser = async (email) => {
     
     return something;
 }
-
-
 
 module.exports = { showTasks, addTask, editTask, deleteTask, getTaskName, addUser, getUser }
